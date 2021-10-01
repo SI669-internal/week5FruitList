9f37ddb2f49ea0ec8898ddc86e24b0342815bf62
Mark Newman <mwnewman@umich.edu>
2021-10-01 00:15:28 -0400

    updated examples for F21

--- App.js
+++ App.js
@@ -33,7 +33,7 @@ export class App1 extends React.Component {
   }
 }
 
-export default class App2 extends React.Component {
+export class App2 extends React.Component {
   constructor(props) {
     super(props);
 
@@ -47,8 +47,7 @@ export default class App2 extends React.Component {
 
   render() {
     return (
-      <View style={styles.container2}>
-        <View>
+        <View style={styles.container2}>
           <FlatList
             data={this.fruit}
             renderItem={({item}) => 
@@ -56,14 +55,43 @@ export default class App2 extends React.Component {
                 <Text style={styles.itemText}>{item.type}</Text>
               </View>
             }
-            style={styles.list}
           />
         </View>
-      </View>
-    );
+  );
   }
 }
 
+export default class App3 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fruit: [
+        { type: 'apple', key: '1'},
+        { type: 'banana', key: '2'},
+        { type: 'grape', key: '3'},
+        { type: 'pineapple', key: '4'},
+        { type: 'mango', key: '5'},
+        { type: 'lychee', key: '6'},
+      ]
+    }
+  }
+
+  render() {
+    return (
+        <View style={styles.container2}>
+          <FlatList
+            data={this.state.fruit}
+            renderItem={({item}) => 
+              <View style={styles.itemContainer}>
+                <Text style={styles.itemText}>{item.type}</Text>
+              </View>
+            }
+          />
+        </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container1: {
@@ -72,19 +100,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: 'white'
   },
+
   container2: {
     flex: 1,
-    alignItems: 'stretch',
-    justifyContent: 'center',
+    paddingTop: '25%',
     backgroundColor: 'grey', 
   },
   itemContainer: {
     flex: 1,
-    backgroundColor: 'lightblue',
-    margin: 10,
-    padding: 15,
+    margin: '5%',
+    padding: '5%',
+    width: '90%',
     alignItems: 'flex-start',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: 'lightblue',
+
   },
   itemText: {
     fontSize: 44,
